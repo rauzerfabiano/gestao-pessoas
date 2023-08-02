a6445cfd941a1c1e82f1dedb9e7e494e3694e000
f <fabiano.rauzer@egsys.com.br>
2023-08-02 16:17:42 -0300

    Criando a rota e a Controller

---
+++ app/Http/Controllers/API/PessoasController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Pessoa;
+use Illuminate\Http\Request;
+
+class PessoasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Pessoa::with('categoria')->paginate(5);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'Nome' => 'required',
+            'CPF' => 'required|unique:pessoas',
+            'E-mail' => 'required|unique:pessoas',
+        ]);
+    
+        $pessoa = Pessoa::create($request->all());
+    
+        return response()->json($pessoa, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pessoa $pessoa)
+    {
+        return $pessoa->load('categoria');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pessoa $pessoa)
+    {
+        $request->validate([
+            'Nome' => 'required',
+        ]);
+    
+        $pessoa->update($request->all());
+    
+        return response()->json($pessoa, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pessoa $pessoa)
+    {
+        $pessoa->delete();
+
+        return response()->json(null, 204);
+    }
+}
